d2532053aeb9d0b639071dbc0997224e063baace
philipp <philipp.dijkstal@psi.ch>
2021-05-18 10:46:45 +0200

    improved start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-#sourcefile=/opt/gfa/python
-#[[ -e $sourcefile ]] && . $sourcefile 3.7
-#export RPN_DEFNS=/afs/psi.ch/user/d/dijkstal_p/bin/defns.rpn
-#export PYTHONPATH=/afs/psi.ch/user/d/dijkstal_p/pythonpath
+sourcefile=/opt/gfa/python
+[[ -e $sourcefile ]] && . $sourcefile 3.7
 [[ $(hostname) == "pc11292.psi.ch" ]] && export PYTHONPATH=$PYTHONPATH:~/pythonpath2/pyqt5
 python3 main.py
